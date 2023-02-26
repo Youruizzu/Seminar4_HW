e530c528427506b6d16fc83b410f9221b3048924
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-26 22:38:50 +0300

    Added Task27 solution

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int Prompt(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InNumSum(int number){
+    int result = 0;
+    while (number > 0){
+        int num = number;
+        result += num%10;
+        number /= 10;
+    }
+    return result;
+}
+
+Console.WriteLine(InNumSum(Prompt("Введите число: ")));
