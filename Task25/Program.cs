61486bb1c522d97e78fb4f3686328a6473c0f16d
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-26 22:04:59 +0300

    Added Task25 solution

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int Prompt(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double Power(double number, double power){
+    double num = number;
+    for(int i = 1; i < power; i++){
+        number *= num;
+    }
+    return number;
+}
+
+Console.WriteLine(Power(Prompt("Введите число: "), Prompt("Введите степень: ")));
