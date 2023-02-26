60dbb29a87c533ff8b57f153730c3419fad88bf0
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-26 22:49:38 +0300

    Added Task29 solution

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+void ShowArray(int size){
+    int[] array = new int[size];
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++){
+        array[i] = new Random().Next(1, 10);
+        Console.Write($"{array[i]}, ");
+    }
+    array[array.Length - 1] = new Random().Next(1, 10);
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
+
+ShowArray(20);
